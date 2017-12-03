a97258a864e0e94b4e21343b1a1ada30760a441f
João Cavalcante <contato@kavalcante.com>
2017-12-03 19:21:38 -0200

    Bump version 1.0.1

--- src/config.js
+++ src/config.js
@@ -1,1 +1,9 @@
+const TOKEN_API = 'BQB0492MxeSRscESp31zl3lAMeJED3UHIL1r8RCEQW6NON4a3B3h-MucHikdeb0kd7h17mVw2iuw3WnwM6Ph81zASGuK0IZIzxbRUmjw2G_10BOJZni1bq6J0hC_vmB3GGtGuNsLmnfbb3E';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+};
+
 export const API_URL = 'https://api.spotify.com/v1';
