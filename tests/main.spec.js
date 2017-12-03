55def231188c5abe329d579f4678aaf4f3dff897
João Cavalcante <contato@kavalcante.com>
2017-12-03 13:27:10 -0200

    generic search added

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,86 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+chai.use(sinonChai);
+sinonStubPromise(sinon);
 
-describe('Main', () => {
+global.fetch = require('node-fetch');
 
+describe('Spotify Wrapper', () => {
+  describe('smoke tests', () => {
+    
+    //search
+    //searchAlbums
+    //searchArtists
+    //searchTracks
+    //searchPlaylists
+
+    it('should exists the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exists the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exists the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exists the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exists the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+
+  describe('Generic Search', () => {
+    let fetchedStub;
+    let promise;
+
+    beforeEach(() => {
+      fetchedStub = sinon.stub(global, 'fetch');
+      promise = fetchedStub.returnsPromise();
+    });
+
+    afterEach(() => {
+      fetchedStub.restore();
+    });
+
+    it('should call fetch function', () => {
+      const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should receive the correct url to fetch', () => {
+      context('passing one type', () => {
+        const artists = search('Muse', 'artist');
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Muse&type=artist');
+
+        const albums = search('Hybryd Theory', 'album');
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Hybryd Theory&type=album');
+      });
+
+      context('passing more than one type', () => {
+        const artistsAndAlbums = search('Incubus', ['artist', 'album']); 
+
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist,album');
+      });
+    });
+
+    it('should return the JSON Data from the Promise', () => {
+      promise.resolves({ body: 'json' });
+      const artists = search('Incubus', 'artist');
+
+      expect(artists.resolveValue).to.be.eql({ body: 'json' });
+    });
+  });
 });
