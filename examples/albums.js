c4d477ec7e9b8738b67d1218611cb43c6c447afb
João Cavalcante <contato@kavalcante.com>
2018-01-07 14:01:43 -0200

    Fix example after refactoring

--- examples/albums.js
+++ examples/albums.js
@@ -1,7 +1,11 @@
+import Wrappify from '../src/index';
+
 global.fetch = require('node-fetch');
 
-import { searchAlbums } from '../src/main';
+const spotify = new Wrappify({
+  token: 'BQBTRNoSbceJBNzwfPdTEwf0Q7seTdt2CXHImmny58U2P1kE9I2COK0GzH3QM46Xp55j6h1X1BKmxaeERHpfjkRfmXrcDOACZUC5eRcu87QF2yAFYi4CPeGwROctfgdnHhXRpyNdU3gtW7rzWQA',
+});
 
-const albums = searchAlbums('Meteora');
+const albums = spotify.search.albums('Meteora');
 
-albums.then(data => console.log(data));
+albums.then(data => data.albums.items.map(item => console.log(item.name)));
